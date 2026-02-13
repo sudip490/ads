f5fd774fc1d460beec23933fa571290f0b6a0b50
subash gautam <subash.gautam@swifttech.com.np>
2026-02-14 02:18:29 +0545

    Update service worker zoneId and remove try-catch

--- public/sw.js
+++ public/sw.js
@@ -1,10 +1,7 @@
+
 self.options = {
     "domain": "3nbf4.com",
-    "zoneId": 10607474
+    "zoneId": 10607520
 }
 self.lary = ""
-try {
-    importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
-} catch (e) {
-    console.log('External service worker script failed to import. This is normal in development environments or if blocked by ad-blockers.');
-}
+importScripts('https://3nbf4.com/act/files/service-worker.min.js?r=sw')
